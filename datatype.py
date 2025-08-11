9ebdd81525891c7b50838b0fa310ae57c6e31152
Aman Prajapati <amanprajapati9359@gmail.com>
2025-08-11 12:23:35 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,13 @@
+#variable me jo value store h o value kis type ki h
+
+name = "Aman"       
+age = 20            
+height = 5.2       
+student = True      
+fruits = ["apple", "banana"]  
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
+print(type(fruits))
